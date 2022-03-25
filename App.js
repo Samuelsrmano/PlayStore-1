ff5bfa0d4e073888c28ebc31005d2b34565b3c71
unknown <you@example.com>
2022-03-25 16:27:49 -0300

    VersaoCabecalho

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Cabecalho from './src/components/cabecalho';
+
+export default function App() {
+  return (
+    <View>
+      <Cabecalho></Cabecalho>
+
+    </View>
+  );
+}
+
+
