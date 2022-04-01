e6a7d4ff4d389ad61cec6865bea5a1a4d0940ffd
Felipe Dourado <felipedourado920@gmail.com>
2022-04-01 16:31:47 -0300

    adição de botoes

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cabecalho from './src/components/cabecalho';
+import Botao from './src/components/botao';
 
 export default function App() {
   return (
     <View>
       <Cabecalho></Cabecalho>
-
+      <Botao
+      Logo= "game-controller"
+      texto="Jogos"
+      cor="black"
+      />
     </View>
   );
 }
